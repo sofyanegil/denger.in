341aaa4ce190625e1cfa82f06aab944b763f3da2
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-03-17 20:16:39 +0700

    update card

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-import $ from "jquery";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/style.css";
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';
 
-import "./script/components/app-bar";
-import main from "./script/view/main";
+import './script/components/app-bar';
+import main from './script/view/main';
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
 
-$("#searchBtn").click(() => {
-  $(".track-list").html(" ");
-  const keyword = $("#searchInput").val();
+$('#searchBtn').click(() => {
+  $('.track-list').html(' ');
+  const keyword = $('#searchInput').val();
   const settings = {
     async: true,
     crossDomain: true,
-    url: "https://deezerdevs-deezer.p.rapidapi.com/search/track",
-    method: "GET",
+    url: 'https://deezerdevs-deezer.p.rapidapi.com/search/track',
+    method: 'GET',
     headers: {
-      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-      "x-rapidapi-key": "0cdbbf3c74mshaa73fb5282ec156p1c0bebjsn8f9dcee65b1b",
+      'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
+      'x-rapidapi-key': '0cdbbf3c74mshaa73fb5282ec156p1c0bebjsn8f9dcee65b1b',
     },
     data: {
       q: keyword,
@@ -28,16 +28,22 @@ $("#searchBtn").click(() => {
       let musik = response.data;
       console.log(musik);
 
-      $.each(musik, function (i, data) {
-        $("#track-list").append(`
+      $('#track-list').html('');
+      $.each(musik, (i, data) => {
+        $('#track-list').append(`
           <div class="card m-3">
             <img src="${data.album.cover_medium}" class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">${data.title}</h5>
-              <p class="card-text">${data.artist.name}.</p>
-              <audio controls>
+              <h4 class="card-title">${data.title}</h4>
+              <p class="card-text">${data.album.title}.</p>
+              <div class="d-flex flex-row justify-content-between">
+                <h3 class="">${data.artist.name}</h3>
+               <img class="rounded-circle border border-3 border-alert-info" width="30" height="30" src="${data.artist.picture_small}" class="card-img-top" alt="..." />
+              </div>
+              <audio controls class="justify-content-center mx-auto">
                 <source src="${data.preview}" type="audio/mpeg">
               </audio>
+              <a href="${data.link}" target=”_blank” rel=”noreferrer noopener” class="btn btn-success w-100">Dengerin di Deezer</a>
             </div>
           </div>
           `);
